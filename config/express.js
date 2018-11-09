46c2d40b2dec90c83e6649e939d66e02563d6e30
Pol Surakitkoson <pol.surakit@gmail.com>
2018-11-09 14:02:51 +0700

    edit init

--- config/express.js
+++ config/express.js
@@ -5,7 +5,14 @@ var bodyParser = require('body-parser');
 
 module.exports = function(){
 	var app = express();
-	
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	} else {
+		app.use(compression);
+	}
+	app.use(bodyParser.urlencoded({
+		extended : true
+	}));
 	app.use(bodyParser.json());
 
 	
