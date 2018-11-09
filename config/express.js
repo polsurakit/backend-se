fd22739f12dc5797c383a9ef2b606a0e322fc40f
Pol Surakitkoson <pol.surakit@gmail.com>
2018-11-09 13:59:50 +0700

    1

--- config/express.js
+++ config/express.js
@@ -5,18 +5,10 @@ var bodyParser = require('body-parser');
 
 module.exports = function(){
 	var app = express();
-	if (process.env.NODE_ENV === 'development') {
-		app.use(morgan('dev'));
-	} else {
-		app.use(compression);
-	}
-	app.use(bodyParser.urlencoded({
-		extended : true
-	}));
+	
 	app.use(bodyParser.json());
 
-	app.set('views', './app/views');
-	app.set('view engine', 'jade');
+	
 	require('../app/routes/index.routes.js')(app); //call function
 
 	app.use(express.static('./public'));
